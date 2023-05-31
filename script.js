dfe37f3e076dec9aa46aeb722c1fad9e7b6defdc
ayush460 <90319070+ayush460@users.noreply.github.com>
2023-05-31 21:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function updateOutputText() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+const outputElement = document.getElementById("output");
+
+updateOutputText()
+  .then((text) => {
+    outputElement.textContent = text;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
